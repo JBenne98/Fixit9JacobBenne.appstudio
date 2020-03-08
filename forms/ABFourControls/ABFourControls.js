48ac7e7575bbec13530957887e7681e23fa4ca58
JBenne98 <jgb83474@creighton.edu>
2020-03-08 16:21:23 -0500

    y

---
+++ forms/ABFourControls/ABFourControls.js
@@ -0,0 +1,18 @@
+
+Signature1.onclick=function(){
+  lblMes.textContent='sign please'
+}
+
+sldOne.onchange=function(){
+  lblMes.textContent='movement'
+}
+
+cbxFour.onclick=function(){
+  let item1='selected'
+  if(item1=='selected'){
+  lblMes.textContent='You choose Item 1'
+}
+
+bcbThree.onresize=function(){
+  lblMes.textContent='IDk'
+}
--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,15 @@
-/*
-var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+var members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+btn1.onclick=function(){
+  let memberInput=inptFirstName.value
+  if{for (i=0;i<members.length;i++){
+    if (memberInput=members[i]){
+     lblOutput.textContent=`You are a member`
+   }
+   }
+   } else {
+     members.push(memberInput)
+     lblOutput.textContent=`You have been added as a member`
+     }
+}
